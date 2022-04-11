bb05d5a418b2d0aea5fe8f94f7e28b4b1b8c46f4
MatveySmir <103235111+MatveySmir@users.noreply.github.com>
2022-04-11 22:12:39 +0300

    Add files via upload

---
+++ FileOperation.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MSApp1.ShellFileOperation;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace MSApp1
+{
+    public static class FileOperation
+    {
+        
+        public enum FileOperations
+        {
+            FO_MOVE = 0x0001,		// Move the files specified in pFrom to the location specified in pTo. 
+            FO_COPY = 0x0002,		// Copy the files specified in the pFrom member to the location specified 
+            // in the pTo member. 
+            FO_DELETE = 0x0003,		// Delete the files specified in pFrom. 
+            FO_RENAME = 0x0004		// Rename the file specified in pFrom. You cannot use this flag to rename 
+            // multiple files with a single function call. Use FO_MOVE instead. 
+        }
+
+        public static FileOperations Operation;
+        public static IntPtr OwnerWindow;
+        public static ShellFileOperationFlags OperationFlags;
+        public static String ProgressTitle;
+
+        public static ShellNameMapping[] NameMappings;
+
+        private static bool MakeOperationsWithFiles(uint Operation,string Source, string Dest)
+        {
+            ShellApi.SHFILEOPSTRUCT FileOpStruct = new ShellApi.SHFILEOPSTRUCT();
+
+            FileOpStruct.hwnd = OwnerWindow;
+            FileOpStruct.wFunc = (uint)Operation;
+
+            FileOpStruct.pFrom = Marshal.StringToHGlobalUni(Source);
+            FileOpStruct.pTo = Marshal.StringToHGlobalUni(Dest);
+
+            FileOpStruct.fFlags = (ushort)OperationFlags;
+            FileOpStruct.lpszProgressTitle = ProgressTitle;
+            FileOpStruct.fAnyOperationsAborted = 0;
+            FileOpStruct.hNameMappings = IntPtr.Zero;
+
+            int RetVal;
+            RetVal = ShellApi.SHFileOperation(ref FileOpStruct);
+
+            ShellApi.SHChangeNotify(
+                (uint)ShellChangeNotificationEvents.SHCNE_ALLEVENTS,
+                (uint)ShellChangeNotificationFlags.SHCNF_DWORD,
+                IntPtr.Zero,
+                IntPtr.Zero);
+
+            if (RetVal != 0)
+                return false;
+
+            if (FileOpStruct.fAnyOperationsAborted != 0)
+                return false;
+
+            return true;
+        }
+
+        public static bool CopyFilesWithMasks(string srcFolder, string dstFolder, string mask)
+        {
+            return MakeOperationsWithFiles(0x0002, MasksToMultiMask(srcFolder, mask), dstFolder);
+        }
+        public static bool MoveFilesWithMasks(string srcFolder, string dstFolder, string mask)
+        {
+            return MakeOperationsWithFiles(0x0001, MasksToMultiMask(srcFolder, mask), dstFolder);
+        }
+        public static bool DeleteFilesWithMasks(string srcFolder,  string mask)
+        {
+            return MakeOperationsWithFiles(0x0003, MasksToMultiMask(srcFolder, mask),null);
+        }
+
+        /// /with List
+
+        public static bool CopyFilesByList(ObservableCollection<string> files, string destFolder)
+        {
+            return MakeOperationsWithFiles(0x0002, StringsToMultiString(files), destFolder);
+        }
+        public static bool MoveFilesByList(ObservableCollection<string> files, string destFolder)
+        {
+            return MakeOperationsWithFiles(0x0001, StringsToMultiString(files), destFolder);
+        }
+        public static bool DeleteFilesByList(ObservableCollection<string> files)
+        {
+            return MakeOperationsWithFiles(0x0003, StringsToMultiString(files),null);
+        }
+
+        private static string StringsToMultiString(ObservableCollection<string> strings)
+        {
+            string str = "";
+
+            // if null or if count == 0 then return empty string
+            if ((strings?.Count ?? 0) == 0)
+                return str;
+
+            for (int i = 0; i < strings.Count; ++i)
+                str += strings[i] + '\0';
+            str += '\0';
+
+            return str;
+        }
+
+        private static string MasksToMultiMask(string sourceFolder, string mask)
+        {
+            ObservableCollection<string> temp = new ObservableCollection<string>();
+            string[] masks;
+            char[] separator = new char[] { '|' };
+
+            masks = mask.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < masks.Length; ++i)
+            {
+                string[] filesWithMask = Directory.GetFiles(sourceFolder, masks[i]);
+                int filesWithMaskCount = filesWithMask.Length;
+
+                for (int j = 0; j < filesWithMaskCount; ++j)
+                    if (!(temp.Contains(filesWithMask[j])))
+                        temp.Add(filesWithMask[j]);
+            }
+            return StringsToMultiString(temp);
+        }
+
+        public static void RenameFile(string Source, string newName)
+        {
+            string dest = Source.Substring(0, Source.LastIndexOf('\\') + 1) + newName;
+            Source += '\0';
+            MakeOperationsWithFiles(0x0004, Source, dest);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> Items;     // ObservableCollection
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Items = new ObservableCollection<string>(); //создаем внутрннее кол-во данных
+            ItemsListBox.ItemsSource = Items;
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FilePathTextBox.Text = "";
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FilePathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPathTextBox.Text = "";
+            var dialog = new FolderBrowserDialog();
+            dialog.Description = "Выбор папки";
+            dialog.ShowNewFolderButton = true;
+            dialog.RootFolder = System.Environment.SpecialFolder.Desktop;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderPathTextBox.Text = dialog.SelectedPath;
+            }
+        }
+        public string FolderPath1;
+        private void SelectFolderButton1_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPathTextBox1.Text = "";
+            var dialog = new FolderBrowserDialog();
+            dialog.Description = "Выбор папки";
+            dialog.ShowNewFolderButton = true;
+            dialog.RootFolder = System.Environment.SpecialFolder.Desktop;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderPathTextBox1.Text = dialog.SelectedPath;
+            }
+            FolderPath1 = FolderPathTextBox1.Text;
+        }
+
+        private void StartFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePathTextBox.Text.Trim() != "" || FilePathTextBox.Text.Trim() != "Файла не существует")
+            {
+                if (File.Exists(FilePathTextBox.Text))
+                {
+                    ShellExecute shellExecute = new ShellExecute();
+                    shellExecute.Verb = ShellExecute.OpenFile;
+                    shellExecute.Path = FilePathTextBox.Text;
+                    shellExecute.Execute();
+                }
+                else
+                {
+                    FilePathTextBox.Text = "Файла не существует";
+                }
+            }
+            else
+            {
+                FilePathTextBox.Text = "Файл не указан";
+            }
+        }
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePathTextBox.Text.Trim() != "" || FilePathTextBox.Text.Trim() != "Файла не существует")
+            {
+                if (File.Exists(FilePathTextBox.Text))
+                {
+                    ShellExecute shellExecute = new ShellExecute();
+                    shellExecute.Verb = ShellExecute.EditFile;
+                    shellExecute.Path = FilePathTextBox.Text;
+                    shellExecute.Execute();
+                }
+                else
+                {
+                    FilePathTextBox.Text = "Файла не существует";
+                }
+            }
+            else
+            {
+                FilePathTextBox.Text = "Файл не указан";
+            }
+        }
+        private void PrintFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePathTextBox.Text.Trim() != "" || FilePathTextBox.Text.Trim() != "Файла не существует")
+            {
+                if (File.Exists(FilePathTextBox.Text))
+                {
+                    ShellExecute shellExecute = new ShellExecute();
+                    shellExecute.Verb = ShellExecute.PrintFile;
+                    shellExecute.Path = FilePathTextBox.Text;
+                    shellExecute.Execute();
+                }
+                else
+                {
+                    FilePathTextBox.Text = "Файла не существует";
+                }
+            }
+            else
+            {
+                FilePathTextBox.Text = "Файл не указан";
+            }
+        }
+
+        private void OpenFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FolderPathTextBox.Text.Trim() != "" || FolderPathTextBox.Text.Trim() != "Папки не существует")
+            {
+                if (File.Exists(FolderPathTextBox.Text))
+                {
+                    ShellExecute shellExecute = new ShellExecute();
+                    shellExecute.Verb = ShellExecute.ExploreFolder;
+                    shellExecute.Path = FolderPathTextBox.Text;
+                    shellExecute.Execute();
+                }
+                else
+                {
+                    FolderPathTextBox.Text = "Папки не существует";
+                }
+            }
+            else
+            {
+                FolderPathTextBox.Text = "Папка не указана";
+            }
+        }
+
+        private void SearchFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FolderPathTextBox.Text.Trim() != "" || FolderPathTextBox.Text.Trim() != "Папки не существует")
+            {
+                if (File.Exists(FolderPathTextBox.Text))
+                {
+                    ShellExecute shellExecute = new ShellExecute();
+                    shellExecute.Verb = ShellExecute.FindInFolder;
+                    shellExecute.Path = FolderPathTextBox.Text;
+                    shellExecute.Execute();
+                }
+                else
+                {
+                    FolderPathTextBox.Text = "Папки не существует";
+                }
+            }
+            else
+            {
+                FolderPathTextBox.Text = "Папка не указана";
+            }
+        }
+
+        private void AddFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePathTextBox.Text != "")
+            {
+                if (Items.IndexOf(FilePathTextBox.Text) == -1)
+                {
+                    if (File.Exists(FilePathTextBox.Text))
+                    {
+                        Items.Add(FilePathTextBox.Text);
+                        ItemsListBox.Items.Refresh();
+                    }
+                }
+            }
+        }
+        private void RemoveListFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Items.RemoveAt(ItemsListBox.SelectedIndex);
+            ItemsListBox.Items.Refresh();
+        }
+        private void ClearListFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Items.Clear();
+            ItemsListBox.Items.Refresh();
+        }
+
+        private void CopyFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.CopyFilesWithMasks(FolderPathTextBox.Text, FolderPathTextBox1.Text, MaskFilePathTextBox.Text);
+        }
+
+        private void MoveFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.MoveFilesWithMasks(FolderPathTextBox.Text, FolderPathTextBox1.Text, MaskFilePathTextBox.Text);
+        }
+
+        private void DeleteFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.DeleteFilesWithMasks(FolderPathTextBox.Text, MaskFilePathTextBox.Text);
+        }
+
+        private void CopyFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.CopyFilesByList(Items, FolderPathTextBox1.Text);
+        }
+
+        private void MoveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.MoveFilesByList(Items, FolderPathTextBox1.Text);
+        }
+
+        private void RemoveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOperation.DeleteFilesByList(Items);
+        }
+
+        private void RenameFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePathTextBox.Text.Trim() != "" || FilePathTextBox.Text.Trim() != "Файла не существует")
+            {
+                if (File.Exists(FilePathTextBox.Text))
+                {
+                    var strVal = Interaction.InputBox("Введите новое имя файла", "Переименование", System.IO.Path.GetFileName(FilePathTextBox.Text));
+                    FileOperation.RenameFile(FilePathTextBox.Text, strVal);
+                }
+            } 
+        }
+    }
+}
